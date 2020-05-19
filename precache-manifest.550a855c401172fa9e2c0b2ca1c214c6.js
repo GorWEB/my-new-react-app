da97a46e87240a87dc359251b1cb0fb2424387e2
GorWEB <brutyan.99@mail.ru>
2020-05-19 15:44:58 +0000

    Updates

---
+++ precache-manifest.550a855c401172fa9e2c0b2ca1c214c6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "457514e4d3213769ec6884e18c1c0451",
+    "url": "/my-new-react-app/index.html"
+  },
+  {
+    "revision": "e67f759477cd53ea0724",
+    "url": "/my-new-react-app/static/css/main.44ef9f40.chunk.css"
+  },
+  {
+    "revision": "0827160fbb70f4ca85c9",
+    "url": "/my-new-react-app/static/js/2.4c3b1664.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/my-new-react-app/static/js/2.4c3b1664.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e67f759477cd53ea0724",
+    "url": "/my-new-react-app/static/js/main.3535f365.chunk.js"
+  },
+  {
+    "revision": "00a53cf10fabc644654b",
+    "url": "/my-new-react-app/static/js/runtime-main.9af8d543.js"
+  },
+  {
+    "revision": "58e4a4e4fa041a11f796a2014b1bcfa4",
+    "url": "/my-new-react-app/static/media/Preloader.58e4a4e4.gif"
+  },
+  {
+    "revision": "580ffd26ccaa16481ed87c1829c74943",
+    "url": "/my-new-react-app/static/media/reactIcon.580ffd26.png"
+  },
+  {
+    "revision": "e22c9c396dc3412121640a83f7e8d19c",
+    "url": "/my-new-react-app/static/media/userImage.e22c9c39.png"
+  }
+]);
